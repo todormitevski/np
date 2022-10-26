f37cbc0062679543fcdae87be1c74f267e6c7440
Todor Mitevski <todormitevski5@gmail.com>
2022-10-26 13:14:20 +0200

    add aud3

---
+++ src/aud3/bank/AccountType.java
@@ -0,0 +1,6 @@
+package aud3.bank;
+
+public enum AccountType {
+    INTEREST,
+    NON_INTEREST
+}
---
+++ src/aud3/bank/InterestBearingAccount.java
@@ -0,0 +1,6 @@
+package aud3.bank;
+
+public interface InterestBearingAccount {
+
+    void addInterest();
+}
