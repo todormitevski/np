56d0ac01a68f4bd56de182606dd958b0cf3d9e2d
TodorM <todormitevski5@gmail.com>
2022-11-10 14:00:54 +0100

    add labs

---
+++ src/labs/banka/FlatAmountProvisionTransaction.java
@@ -0,0 +1,24 @@
+package labs.banka;
+
+public class FlatAmountProvisionTransaction extends Transaction{
+    private final String flatAmount;
+
+    public FlatAmountProvisionTransaction(long fromId, long toId, String amount, String flatProvision) {
+        super(fromId, toId, "FlatAmount", amount);
+        this.flatAmount = flatProvision;
+    }
+
+    @Override
+    public String provision() {
+        return flatAmount;
+    }
+
+    public String getFlatAmount() {
+        return flatAmount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+}
---
+++ src/labs/banka/Transaction.java
@@ -0,0 +1,48 @@
+package labs.banka;
+
+import java.util.Objects;
+
+public abstract class Transaction {
+    protected long fromId;
+    protected long toId;
+    protected String description;
+    protected String amount; //transaction amount
+
+    public Transaction(long fromId, long toId, String description, String amount) {
+        this.fromId = fromId;
+        this.toId = toId;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public long getFromId() {
+        return fromId;
+    }
+
+    public long getToId() {
+        return toId;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract String provision();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transaction that = (Transaction) o;
+        return fromId == that.fromId && toId == that.toId && Objects.equals(description, that.description) && Objects.equals(amount, that.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fromId, toId, description, amount);
+    }
+}
